17d4912e38fee1c2cbeef5762972a418cd190a4f
Marcin Korbecki <marcin.korbecki@mayeryn.com>
2023-02-13 13:48:34 +0100

    update project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './task'
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   config: { [key: string]: string | Date; } | undefined ;
+  tasks: Task[] = [];
 
   constructor() {
     setTimeout(()=> {
